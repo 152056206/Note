2ca06a0034d2aeaf40b9ffe65f1c0a4992abc78e
152056206 <2028393309@qq.com>
2017-12-20 18:02:18 +0800

    note

---
+++ 记事本-李昊华/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace 记事本_李昊华
+{
+    partial class frmNotepad
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnusNotepad = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.timiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtxtNotepad = new System.Windows.Forms.RichTextBox();
+            this.sdlgNotepad = new System.Windows.Forms.SaveFileDialog();
+            this.odlgNotepad = new System.Windows.Forms.OpenFileDialog();
+            this.mnusNotepad.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnusNotepad
+            // 
+            this.mnusNotepad.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile});
+            this.mnusNotepad.Location = new System.Drawing.Point(0, 0);
+            this.mnusNotepad.Name = "mnusNotepad";
+            this.mnusNotepad.Size = new System.Drawing.Size(584, 25);
+            this.mnusNotepad.TabIndex = 0;
+            this.mnusNotepad.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNew,
+            this.timiOpen,
+            this.tsmiSave,
+            this.tsmiSaveAs,
+            this.tsmiClose});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(44, 21);
+            this.tsmiFile.Text = "文件";
+            // 
+            // tsmiNew
+            // 
+            this.tsmiNew.Name = "tsmiNew";
+            this.tsmiNew.Size = new System.Drawing.Size(112, 22);
+            this.tsmiNew.Text = "新建";
+            this.tsmiNew.Click += new System.EventHandler(this.tsmiNew_Click);
+            // 
+            // timiOpen
+            // 
+            this.timiOpen.Name = "timiOpen";
+            this.timiOpen.Size = new System.Drawing.Size(112, 22);
+            this.timiOpen.Text = "打开";
+            this.timiOpen.Click += new System.EventHandler(this.timiOpen_Click);
+            // 
+            // tsmiSave
+            // 
+            this.tsmiSave.Name = "tsmiSave";
+            this.tsmiSave.Size = new System.Drawing.Size(112, 22);
+            this.tsmiSave.Text = "保存";
+            this.tsmiSave.Click += new System.EventHandler(this.tsmiSave_Click);
+            // 
+            // tsmiSaveAs
+            // 
+            this.tsmiSaveAs.Name = "tsmiSaveAs";
+            this.tsmiSaveAs.Size = new System.Drawing.Size(112, 22);
+            this.tsmiSaveAs.Text = "另存为";
+            this.tsmiSaveAs.Click += new System.EventHandler(this.tsmiSaveAs_Click);
+            // 
+            // tsmiClose
+            // 
+            this.tsmiClose.Name = "tsmiClose";
+            this.tsmiClose.Size = new System.Drawing.Size(112, 22);
+            this.tsmiClose.Text = "退出";
+            this.tsmiClose.Click += new System.EventHandler(this.tsmiClose_Click);
+            // 
+            // rtxtNotepad
+            // 
+            this.rtxtNotepad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtxtNotepad.Location = new System.Drawing.Point(12, 28);
+            this.rtxtNotepad.Name = "rtxtNotepad";
+            this.rtxtNotepad.Size = new System.Drawing.Size(560, 372);
+            this.rtxtNotepad.TabIndex = 1;
+            this.rtxtNotepad.Text = "";
+            this.rtxtNotepad.TextChanged += new System.EventHandler(this.rtxtNotepad_TextChanged);
+            // 
+            // sdlgNotepad
+            // 
+            this.sdlgNotepad.FileName = "无标题";
+            this.sdlgNotepad.Filter = "RTF文件|*.rtf";
+            // 
+            // odlgNotepad
+            // 
+            this.odlgNotepad.FileName = "openFileDialog1";
+            this.odlgNotepad.Filter = "RTF文件|*.rtf|所有文件|*.*";
+            // 
+            // frmNotepad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 412);
+            this.Controls.Add(this.rtxtNotepad);
+            this.Controls.Add(this.mnusNotepad);
+            this.MainMenuStrip = this.mnusNotepad;
+            this.Name = "frmNotepad";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "152056206李昊华";
+            this.mnusNotepad.ResumeLayout(false);
+            this.mnusNotepad.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnusNotepad;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNew;
+        private System.Windows.Forms.ToolStripMenuItem timiOpen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSave;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem tsmiClose;
+        private System.Windows.Forms.RichTextBox rtxtNotepad;
+        private System.Windows.Forms.SaveFileDialog sdlgNotepad;
+        private System.Windows.Forms.OpenFileDialog odlgNotepad;
+    }
+}
+
